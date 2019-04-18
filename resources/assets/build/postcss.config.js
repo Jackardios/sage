1ab0bd1b2e595ecedb3c30a045d707cb70ec9a31
Jackardios <salahutdinov.salavat@gmail.com>
2019-04-18 16:27:22 +0300

    Add tailwindcss instead bootstrap

--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const tailwindcssConfig = require('../../../tailwind');
 const cssnanoConfig = {
   preset: ['default', { discardComments: { removeAll: true } }],
 };
@@ -8,10 +9,10 @@ module.exports = ({ file, options }) => {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
       cssnano: options.enabled.optimize ? cssnanoConfig : false,
-      autoprefixer: true,
-      'css-mqpacker': { sort: true },
-      'postcss-flexbugs-fixes': true,
-      // 'postcss-normalize': true,
+      autoprefixer: options.enabled.optimize,
+      'css-mqpacker': options.enabled.optimize ? { sort: true } : false,
+      'postcss-flexbugs-fixes': options.enabled.optimize,
+      tailwindcss: tailwindcssConfig,
     },
   };
 };
