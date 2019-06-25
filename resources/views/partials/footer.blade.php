4ffcb6006cd1a0c4681fc5e9c7d4c57d7c966957
Jackardios <salahutdinov.salavat@gmail.com>
2019-06-25 22:25:47 +0300

    Add polyfill script

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,3 +3,4 @@
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
+<script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?features=Object.assign%2CObject.values%2CElement.prototype.classList%2CElement.prototype.matches%2CElement.prototype.closest%2CrequestAnimationFrame"></script>
